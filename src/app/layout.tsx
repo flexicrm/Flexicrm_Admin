d95c5f3f92f1ada537e75a2925bc48a60b94f73c
Ajin.T.S <149351487+Ajints@users.noreply.github.com>
2025-06-10 15:33:29 +0530

    build error clear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -133,7 +133,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="apple-touch-icon" sizes="60x60" href="/logo/apple-icon-60x60.png" />
                 <link rel="apple-touch-icon" sizes="72x72" href="/logo/apple-icon-72x72.png" />
                 <link rel="apple-touch-icon" sizes="76x76" href="/logo/apple-icon-76x76.png" />
-                <link rel="apple-touch-icon" sizes="114x114" href="/logo/apple-icon-114x114.png" />
+                {/* <link rel="apple-touch-icon" sizes="114x114" href="/logo/apple-icon-114x114.png" /> */}
                 <link rel="apple-touch-icon" sizes="120x120" href="/logo/apple-icon-120x120.png" />
                 {/* <link rel="apple-touch-icon" sizes="144x144" href="/logo/apple-icon-144x144.png" /> */}
                 <link rel="apple-touch-icon" sizes="152x152" href="/logo/apple-icon-152x152.png" />
